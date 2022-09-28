345e5418dc2081e60846204c21e2f9abf82f59a5
RamFarid <workprojects22@gmail.com>
2022-09-28 05:27:57 +0200

    Update my PDF CV

--- src/Components/Download.jsx
+++ src/Components/Download.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import resume from '../Assets/Ram_farid_resume.pdf'
+import resume from '../Assets/Ram_Farid_CV.pdf'
 function Download() {
   return (
-    <a className='secondary-btn' href={resume} download>
+    <a className='secondary-btn' href={resume} download='Ram_Farid_CV.pdf'>
       Download Resume
     </a>
   )
