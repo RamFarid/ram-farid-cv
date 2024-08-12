af8afc25898fa6a8a529168e6daa38727ad9eaa7
RamFarid <workprojects22@gmail.com>
2024-08-12 04:12:56 +0300

    Fix the description table and 'code'

--- src/components/Portfolio/MarkDown.jsx
+++ src/components/Portfolio/MarkDown.jsx
@@ -3,7 +3,20 @@ import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
 function MarkDown({ desc }) {
-  return <ReactMarkdown remarkPlugins={[remarkGfm]}>{desc}</ReactMarkdown>
+  return (
+    <ReactMarkdown
+      remarkPlugins={[remarkGfm]}
+      components={{
+        table: ({ node, ...props }) => (
+          <div className='table-wrapper'>
+            <table {...props} />
+          </div>
+        ),
+      }}
+    >
+      {desc}
+    </ReactMarkdown>
+  )
 }
 
 export default MarkDown
