3abe76cc7f85c32087fc384183372ccf53298b66
RamFarid <workprojects22@gmail.com>
2024-08-12 03:50:30 +0300

    Make the gallery view photo

--- src/components/Portfolio/ProjectGallery.jsx
+++ src/components/Portfolio/ProjectGallery.jsx
@@ -2,27 +2,46 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/effect-cards'
+import 'react-photo-view/dist/react-photo-view.css'
 import { EffectCards } from 'swiper'
+import { PhotoSlider } from 'react-photo-view'
+import { Fragment, useState } from 'react'
 
 function ProjectGallery({ images }) {
-  console.log(images)
+  const [sliderIndex, setSliderIndex] = useState(0)
+  const [isSliderVisible, setIsSliderVisible] = useState(false)
   return (
-    <Swiper
-      effect='cards'
-      grabCursor={true}
-      modules={[EffectCards]}
-      slidesPerView={1}
-    >
-      {images &&
-        images.map((img, index) => {
-          return (
-            <SwiperSlide key={index}>
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img src={img} alt={'project.title'} />
-            </SwiperSlide>
-          )
-        })}
-    </Swiper>
+    <Fragment>
+      <PhotoSlider
+        images={images.map((img) => ({ src: img, key: img }))}
+        visible={isSliderVisible}
+        onClose={() => setIsSliderVisible(false)}
+        index={sliderIndex}
+        onIndexChange={setSliderIndex}
+      />
+      <Swiper
+        effect='cards'
+        grabCursor={true}
+        modules={[EffectCards]}
+        slidesPerView={1}
+      >
+        {images &&
+          images.map((img, index) => {
+            return (
+              <SwiperSlide
+                key={index}
+                onClick={() => {
+                  setSliderIndex(index)
+                  setIsSliderVisible(true)
+                }}
+              >
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img src={img} alt={'project.title'} />
+              </SwiperSlide>
+            )
+          })}
+      </Swiper>
+    </Fragment>
   )
 }
 
